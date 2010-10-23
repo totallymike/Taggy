cd8f412b8b9791be7eb174e054eeb6170b654a1a
Michael Westbom <totallymike@gmail.com>
2010-10-23 00:48:58 -0500

    Added a few new functions to audio_base; initial checkin for audio_mp3.py

--- taggy/audio_base.py
+++ taggy/audio_base.py
@@ -2,23 +2,51 @@ class audio_base:
     """Base class to be inherited by other audio types (e.g. mp3 or flac types).
 
     Includes a number of methods to be overwritten by inheriting classes:
-    get_tags():      Return a dictionary of the tags embedded within a given
-                     file, titled in a friendly way.
-                     e.g. ['Title': 'Money', 'Artist': 'Pink Floyd']
-    get_tags_raw():  Return a dictionary of the tags embedded within a given
-                     file titled as they are in their individual specs.
-                     e.g. ['TIT2': 'Money', 'TPE2': 'Pink Floyd'] (for ID3)
-    clear_tags()     Delete all tags and metadata from the file.
+    print_tags():
+        Print tags embedded in file in a human-readable fashion, e.g.:
+            Title: Money
+            Artist: Pink Floyd
+    print_tags_raw():
+        Print tags embedded in file as labelled in specification, e.g.:
+            TIT2: Money
+            TPE2: Pink Floyd
+        for ID3 format.
+    clear_tags():
+        Delete all tags and metadata from the file.
+    set_raw_tag(tag, value):
+        Define given raw tag (e.g. TIT2 for ID3) with value.
+    set_title(value):
+    set_album(value):
+    set_artist(value):
+    set_album_artist(value):
+    set_genre(value):
+    set_track_number(value):
+    set_year(value):
+        Define corresponding tag with specified value.
     """
     def __init__(self, file_path):
         """Establishes the filename"""
         self.file_name = file_path
     
+    def set_raw_tag():
+        raise NotImplementedError("Function undefined.")
     def get_tags():
-        raise NotImplementedError("Function undefined")
-
+        raise NotImplementedError("Function undefined.")
     def get_tags_raw():
-        raise NotImplementedError("Function undefined")
-
+        raise NotImplementedError("Function undefined.")
     def clear_tags():
-        raise NotImplementedError("Function undefined")
+        raise NotImplementedError("Function undefined.")
+    def set_title():
+        raise NotImplementedError("Function undefined.")
+    def set_album():
+        raise NotImplementedError("Function undefined.")
+    def set_artist():
+        raise NotImplementedError("Function undefined.")
+    def set_album_artist():
+        raise NotImplementedError("Function undefined.")
+    def set_genre():
+        raise NotImplementedError("Function undefined.")
+    def set_track_number():
+        raise NotImplementedError("Function undefined.")
+    def set_year():
+        raise NotImplementedError("Function undefined.")
