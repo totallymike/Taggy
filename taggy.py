1c4c9581d97b0f57ca585a800143652f0f8177df
Michael Westbom <totallymike@gmail.com>
2010-10-23 00:51:55 -0500

    Minor commenting cleanup.

--- taggy.py
+++ taggy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# A command-line ID3 tagger for various audio file formats.  extremely incomplete.
+# A command-line ID3 tagger for various audio file formats.  Extremely incomplete.
 
 import os.path
 from glob import glob
@@ -7,15 +7,17 @@ from optparse import OptionParser
 from mutagen.mp3 import MP3
 from mutagen.flac import FLAC
 from mutagen import id3
+
 def main():
 
-##             Option parsing.
+## Option parsing.
     usage = u"taggy.py [options] filename(s)"
     parser = OptionParser(usage=usage)
     parser.add_option("-l", "--list", action="store_const", const="list",
                       help="list tags embedded in a file", dest="command", default=False)
     (options, args) = parser.parse_args()
 
+    
     
 if __name__ == "__main__":
     main()
