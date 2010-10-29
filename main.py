87be4627430784a28f09452b872a47369a514493
Michael Westbom <totallymike@gmail.com>
2010-10-29 01:13:42 -0500

    Started work on the list function.  Removed mutagen imports from main.py
    Importing mutagen is unnecessary; handler functions obscure mutagen access.

--- main.py
+++ main.py
@@ -3,21 +3,26 @@
 
 import sys
 import os.path
+from tag import ID3
 from glob import glob
 from optparse import OptionParser
-from mutagen.mp3 import MP3
-from mutagen.flac import FLAC
-from mutagen import id3
 
+
+def list(options, files):
+    """List the files."""
+    if options.raw:
+        
 def main():
 
 ## Option parsing.
     usage = u"taggy.py [options] filename(s)"
     parser = OptionParser(usage=usage)
     parser.add_option("-l", "--list", action="store_const", const="list",
-                      help="list tags embedded in a file", dest="command", default=False)
+                      help="list tags embedded in a file", dest="command")
+    parser.add_option('-r', '--raw', action="store_true", dest='raw', default=False)
     (options, args) = parser.parse_args()
 
+    # Generate files list.
     files = []
     for item in args:
         if os.path.exists(item):
@@ -26,7 +31,7 @@ def main():
             print "File " + item + " not found."
             sys.exit(1)
     
-    command(options, args)
+    options.command(options, files)
 
     
 if __name__ == "__main__":
